9dba5e59880a5365001077ccbc838d3cab983b5d
Rohit Tiwari <rohit.tiwari968@gmail.com>
2020-10-22 19:44:27 +0530

    Airbnb Frontend Clone

---
+++ build/precache-manifest.31e093012756fa1c9af8566666529d6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab198aeac3c9090398f87026b305d8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "299b1e8651bbf24a8b36",
+    "url": "/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "63c2833b574981fdd86b",
+    "url": "/static/css/main.9c537269.chunk.css"
+  },
+  {
+    "revision": "299b1e8651bbf24a8b36",
+    "url": "/static/js/2.40d3fd26.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.40d3fd26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63c2833b574981fdd86b",
+    "url": "/static/js/main.650031df.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
